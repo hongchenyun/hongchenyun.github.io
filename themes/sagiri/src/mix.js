b38fb273ef2921ae24076f3d9a27442952c47f71
hongchenyun <2222221973@qq.com>
2018-05-04 21:27:36 +0800

    1

--- themes/sagiri/src/mix.js
+++
@@ -1,7 +0,0 @@
-// 顶部头像动画
-$('.site-master-avatar').on('mouseover', function () {
-  this.classList.add('animated', 'tada');
-});
-$('.site-master-avatar').on('mouseout', function () {
-  this.classList.remove('animated', 'tada');
-});
